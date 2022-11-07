d44feb88f6ddf7de8fed51204a805fe852eddaa1
YangDoris <115921634+YangDoris@users.noreply.github.com>
2022-11-07 20:28:30 +0800

    Add files via upload

---
+++ class2/test-js.js
@@ -0,0 +1,26 @@
+function Yaha(){
+    console.log("YAYAYAHAHAHA 10/27")
+}
+
+Yaha();
+let x = 20;
+let y = 30;
+let z;
+console.log("x=",x);
+console.log("y=",y);
+z = x;
+x = y;
+y = z;
+console.log("swift x and y")
+console.log("x=",x);
+console.log("y=",y);
+
+console.log("*************************");
+console.log("11/3");
+const users=["Da","JO","AB"];
+const newUser1="DA";
+const newUser2="JO";
+const foundNewUser1 = users.indexOf(newUser1);
+console.log(foundNewUser1);
+const foundNewUser2 = users.indexOf(newUser2);
+console.log(foundNewUser2);
