5385d5f5d01d52cb7b0cca99f93c73db81c0a58c
Doris Yang <starship520@gmail.com>
2022-11-08 20:55:49 +0800

    homework 20221103

---
+++ class3/multiplication-table.js
@@ -0,0 +1,10 @@
+function MultiplicationTable(){
+    for (num4=1 ; num4<10 ; num4++){
+        for (num5=1 ; num5<10 ; num5++){
+
+            console.log(`${num4} * ${num5} = `,num4*num5)
+        }
+    }
+}
+
+MultiplicationTable()
---
+++ class3/odd-or-even-number.js
@@ -0,0 +1,6 @@
+let num1 = 4400
+if ( num1 % 2 == 0) {
+    console.log(num1,'= 偶數')
+}else {
+    console.log(num1,'= 奇數')
+}
